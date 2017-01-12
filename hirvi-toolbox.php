361465008e71ec7eb4ae77d29b3b2d8dd5b9616b
Chris Magnussen <chris@hirvi.no>
2017-01-12 13:38:50 +0100

    Updated

--- hirvi-toolbox.php
+++ hirvi-toolbox.php
@@ -1,8 +1,17 @@
 <?php
 /**
- *
+ * Plugin Name: HWP - Toolbox
+ * Plugin URI: https://hirvi.no
+ * Description: A toolbox for WP development. Required by all our plugins and themes.
+ * Author: Chris Magnussen
+ * Author URI: https://hirvi.no/team/chris
+ * Version: 0.1
+ * Text Domain: vc-utils
+ * License: GPLv2
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  */
 
+require_once __DIR__ . '/vendor/autoload.php';
 
 /**
  * Load dependencies
